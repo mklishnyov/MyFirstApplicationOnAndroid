66cb585ef1637686669b8386ffd43494afc17a3d
Matvey <klishnev2018@gmail.com>
2025-09-30 10:38:36 +0500

    Initial commit

---
+++ app/src/main/java/com/example/myfirstapplicationonandroid/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.myfirstapplicationonandroid
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.myfirstapplicationonandroid.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        binding.btnTextChange.setOnClickListener {
+            binding.textField.text = "Button Clicked"
+        }
+
+    }
+}
